a3cbbd3bb5c7515dd70c1a00e8cf0818f25786ec
Ashwin <aswinjeganath@gmail.com>
2019-07-17 08:54:19 +0530

    mock server v0.1

---
+++ index.js
@@ -0,0 +1,81 @@
+require('dotenv').config()
+
+const fastify = require('fastify')({ logger: true })
+const Datastore = require('nedb')
+const db = new Datastore()
+
+fastify.get('/api/users', {
+  schema: {
+    response: {
+      200: {
+        type: 'array',
+        items: {
+          type: 'object',
+          properties: {
+            firstName: { type: 'string' },
+            lastName: { type: 'string' },
+          },
+        },
+      },
+    },
+  },
+  handler: (req, res) => {
+    db.find({}, (err, docs) => {
+      if (err) {
+        console.log(err)
+        return res.send(err)
+      }
+      console.log(docs)
+      res.send(docs)
+    })
+  },
+})
+
+fastify.post('/api/users', {
+  schema: {
+    body: {
+      type: 'object',
+      properties: {
+        firstName: { type: 'string' },
+        lastName: { type: 'string' },
+      },
+      required: ['firstName', 'lastName'],
+    },
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          firstName: { type: 'string' },
+          lastName: { type: 'string' },
+        },
+      },
+    },
+  },
+  handler: (req, res) => {
+    console.log(req.body)
+    db.insert(req.body, (err, doc) => {
+      if (err) {
+        console.log(err)
+        return res.status(500).send(err)
+      }
+      console.log('done')
+      res.send(doc)
+    })
+  },
+})
+
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+    console.log(
+      `Server listening on: http://${fastify.server.address().address}:${
+        fastify.server.address().port
+      }`
+    )
+  } catch (err) {
+    console.log(err)
+    process.exit(1)
+  }
+}
+
+start()
